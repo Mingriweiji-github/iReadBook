f73a3d4f98357a3e08c9ad4e2f0040f2c7fc9b05
KM <18610929200@163.com>
2018-06-25 16:55:14 +0800

    Initial

---
+++ iReadBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iReadBook
+//
+//  Created by Roc01 on 2018/6/25.
+//  Copyright © 2018年 Roc.iMac01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
